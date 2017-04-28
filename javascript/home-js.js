2ed4eeb9dbea02264c199c17c15f8b0ba52a419f
Ivanr553 <ivanr553@gmail.com>
2017-04-28 01:19:37 -0700

    changed github merge additions

--- javascript/home-js.js
+++ javascript/home-js.js
@@ -1,9 +1,6 @@
 function galleryLink() {
   window.open("Gallery.html", "_self");
 }
-<<<<<<< HEAD
 function openAboutMe(){
   window.open("bio.html", "_self");
 }
-=======
->>>>>>> 27dcaab323dfe22dc034a58a0e3fa76f6677ae3b
