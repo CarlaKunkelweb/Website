2f40c35bfa77282d16298bb8d933c21a12d4b5cf
Ivan Raffucci <ivanr553@gmail.com>
2017-05-21 13:12:15 -0700

    finished home page

--- javascript/home-js.js
+++ javascript/home-js.js
@@ -4,3 +4,39 @@ function galleryLink() {
 function openAboutMe(){
   window.open("bio.html", "_self");
 }
+
+// display elements by scrolltop functionality
+if($(window).width() > 1000) {
+  $(window).scroll( () => {
+    if($(window).scrollTop() > ($("#img-1").offset().top-($("#img-1").height()/1.5))) {
+      $("#img-1").css({
+        "transform" : "translatex(0)",
+        "animation" : "fade-in 1.5s forwards"
+      })
+      $("#img-2").css({
+        "transform" : "translatex(0)",
+        "animation" : "fade-in 1.5s forwards"
+      })
+    }
+    if($(window).scrollTop() > ($("#img-3").offset().top*0.7)) {
+      $("#img-3").css({
+        "animation" : "fade-in 1.5s forwards"
+      })
+    }
+    if($(window).scrollTop() > ($("#img-4").offset().top*0.75)) {
+      $("#img-4").css({
+        "transform" : "translatex(0)",
+        "animation" : "fade-in 1.5s forwards"
+      })
+      $("#img-5").css({
+        "transform" : "translatex(0)",
+        "animation" : "fade-in 1.5s forwards"
+      })
+    }
+    if($(window).scrollTop() > ($("#img-6").offset().top*0.8)) {
+      $("#img-6").css({
+        "animation" : "fade-in 1.5s forwards"
+      })
+    }
+  })
+}
