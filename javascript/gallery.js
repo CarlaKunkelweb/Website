3d6393b25a7f5ad8cb8d9b5d16bfa61ec6e739e3
Ivanr553 <ivanr553@gmail.com>
2017-08-13 16:31:11 -0700

    changes to gallery and added domain name

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -4,26 +4,55 @@ function openHome() {
 
 //Album Functionality
 function showAlbum(album) {
-  $("#body-container").css({
-    "display" : "none"
-  })
-  $("#albums-container").css({
-    "display" : "block"
-  })
-  $("#back-to-albums").css({
-    "display" : "block"
-  })
+  function changeBody() {
+    $("#body-container").css({
+      "display" : "none"
+    })
+    $("#albums-container").css({
+      "display" : "block"
+    })
+    $("#back-to-albums").css({
+      "display" : "block"
+    })
+    $(".album").css({
+      "animation" : ""
+    })
+  }
   if(album == "Corkwood") {
-    $("#album-corkwood").removeClass("hide")
+    $("#album-card-corkwood").css({
+      "animation" : "grey-out 0.8s forwards"
+    })
+    setTimeout(function() {
+      $("#album-corkwood").removeClass("hide")
+      changeBody()
+    }, 1000)
   }
   if(album == "Money") {
+  $("#album-card-money").css({
+    "animation" : "grey-out 0.8s forwards"
+  })
+  setTimeout(function() {
     $("#album-money").removeClass("hide")
+    changeBody()
+  }, 1000)
   }
   if(album == "Duval") {
+  $("#album-card-duval").css({
+    "animation" : "grey-out 0.8s forwards"
+  })
+  setTimeout(function() {
     $("#album-duval").removeClass("hide")
+    changeBody()
+  }, 1000)
   }
   if(album == "Extra") {
+  $("#album-card-extra").css({
+    "animation" : "grey-out 0.8s forwards"
+  })
+  setTimeout(function() {
     $("#album-extra").removeClass("hide")
+    changeBody()
+  }, 1000)
   }
 }
 
@@ -43,7 +72,7 @@ function backToAlbums() {
 function enlargeImage(img) {
   if($(window).width() > 1100){
     if($("#" + img.id).hasClass("album-content-img-centered")) {
-      $("body").one("click", ()=> {
+      $("body").one("click", function() {
         $("#" + img.id).removeClass("album-content-img-centered")
         $("#enlarged-image-shader").css({
           "display" : "none"
